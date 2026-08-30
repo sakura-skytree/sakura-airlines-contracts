45688ef1475fa629430a2daf24eb348b647ab39e
AIFuzi <eduard.li2017.ooo@yandex.ru>
2026-08-30 22:54:04 +0300

    feat: basic airport proto

--- src/proto/paths.ts
+++ src/proto/paths.ts
@@ -5,4 +5,5 @@ export const PROTO_PATCH = {
 	SESSION: join(__dirname, '../../proto/session/v1/session.proto'),
 	USER: join(__dirname, '../../proto/user/v1/user.proto'),
 	PASSENGER: join(__dirname, '../../proto/passenger/v1/passenger.proto'),
+	AIRPORT: join(__dirname, '../../proto/airport/v1/airport.proto'),
 } as const
